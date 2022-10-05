0f851ff601b8b16c3bf3c11dbc480cfe9c5ebe6f
perforb <dev.perfumed.garden@gmail.com>
2022-10-05 23:04:42 +0900

    Refactor

--- golang/cmd/chap01/answer_B02.go
+++ golang/cmd/chap01/answer_B02.go
@@ -4,13 +4,12 @@ import "fmt"
 
 func main() {
 	var a, b int
-	found := false
 	_, _ = fmt.Scan(&a, &b)
-	for a <= b {
+	found := false
+	for ; a <= b; a++ {
 		if 100%a == 0 {
 			found = true
 		}
-		a++
 	}
 	if found {
 		fmt.Println("Yes")
